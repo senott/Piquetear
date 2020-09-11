a55b632fa75cf5d3834b6ea7bf41debc9127653b
Senott <senott@yahoo.com>
2020-09-11 08:48:14 -0300

    Project renamed.

--- ios/PiquetearTests/PiquetearTests.m
+++ ios/PiquetearTests/PiquetearTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativetemplateTests : XCTestCase
+@interface PiquetearTests : XCTestCase
 
 @end
 
-@implementation reactnativetemplateTests
+@implementation PiquetearTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
